df658e2fa1d3f456c213481cdc447aeb24e1b888
sunserigabby-byte <sunserigabby@gmail.com>
2025-11-16 20:01:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -277,6 +277,11 @@ const TEAM_MEMBERS = [
   // Example:
   // { name: 'Gabby Sunseri', photo: 'images/gabby.jpg' },
   // { name: 'Kyle Warzecha', photo: 'images/kyle.jpg' },
+  { name: 'Gabby Sunseri', photo: 'gabby.jpg' },
+  { name: 'Jon Lamb', photo: 'jon.jpg' },
+  { name: 'Gio Ramirez', photo: 'gio.jpg' },
+  { name: 'Caity and Cole', photo: 'caitycole.jpg' },
+  { name: 'Kyle Warzecha', photo: 'kyle.jpg' },
 ];
 
 function shuffleArray(arr) {
