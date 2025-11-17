3de83a79ee4240bd7a7e867be27694ca6762f344
sunserigabby-byte <sunserigabby@gmail.com>
2025-11-16 22:03:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -254,11 +254,10 @@ function setupRSVP() {
     let text = `YAY! RSVP received for ${name}`;
     if (plusOne) text += ` + ${plusOne}`;
     if (attending === 'no') {
-      text += `. Thanks for letting us know — we’ll miss you!`;
+      text += `. Thanks for letting us know — we’ll miss you! Hopefully we will see you next time!`;
     } else {
-      text += `. If you are attending, please Venmo $${amount} to @Kyle-Warzecha to confirm your spot.`;
-      text += ` Refund policy: If you need to cancel your RSVP, we can provide a full refund with at least 1 week's notice. `;
-      text += `No refunds are available within 48 hours of the event.`;
+      text += `. If you change your mind, you can RSVP again through out website and Venmo $${amount} to @Kyle-Warzecha to confirm your spot.`;
+      text += ` We would love your thoughts on our event / why you may not be attending! We know this may not be everyone's idea of fun, but we hope to make it better next time! If you have a moment, please fill our this anonymous survey: https://forms.gle/MTLCKoYo5FW9drS27 `;
     }
     msg.textContent = text;
 
